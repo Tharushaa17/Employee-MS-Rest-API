3741c471065d333881b5758917047b70e41653ac
tharushaa17 <tharushaa.td17@gmail.com>
2024-01-12 11:20:49 +0530

    Created the update get search delete method

--- src/main/java/com/example/employees/service/EmployeeService.java
+++ src/main/java/com/example/employees/service/EmployeeService.java
@@ -6,9 +6,13 @@ import com.example.employees.repo.EmployeeRepo;
 import com.example.employees.utils.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -34,7 +38,31 @@ public class EmployeeService {
             employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
             return VarList.RSP_SUCCESS;
         }else {
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
 
+    public List<EmployeeDTO> getAllEmployee() {
+        List<Employee> employeeList = employeeRepo.findAll();
+        return modelMapper.map(employeeList, new TypeToken<ArrayList<EmployeeDTO>>(){
+        }.getType());
+    }
+
+    public EmployeeDTO searchEmployee(int empId){
+        if (employeeRepo.existsById(empId)){
+            Employee employee = employeeRepo.findById(empId).orElse(null);
+            return modelMapper.map(employee, EmployeeDTO.class);
+        }else {
+            return null;
+        }
+    }
+
+    public String deleteEmployee(int empID){
+        if (employeeRepo.existsById(empID)){
+            employeeRepo.deleteById(empID);
+            return VarList.RSP_SUCCESS;
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
         }
     }
 }
